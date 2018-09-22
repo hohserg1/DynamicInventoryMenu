f5d6438e55b278f75bccef99c2e095965bb17b07
hohserg <hohserg1@yandex.ru>
2018-09-22 20:44:49 +0300

    Make DataSource is polymorphic

--- src/main/scala/hohserg/inventorymenu/menu/DataSource.scala
+++ src/main/scala/hohserg/inventorymenu/menu/DataSource.scala
@@ -1,40 +1,36 @@
 package hohserg.inventorymenu.menu
 
-import hohserg.inventorymenu.notify.{Notified, Observable, OpenObservable}
+import hohserg.inventorymenu.notify.{Observable, Pipe}
 import org.bukkit.inventory.ItemStack
 
-import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
-trait DataSource extends Observable {
-  def getItem: ItemStack
+trait DataSource[A] extends Observable with Pipe {
+  def getItem: A
 }
 
-case class SelectedSource[A, B](map: OpenObservable[A, B], index: Int, visualize: ((A, B)) => ItemStack) extends DataSource with Notified {
-  map.addNotified(this)
+case class SelectedSource[A](collection: TraversableOnce[A] with Observable, index: Int, visualize: A => ItemStack) extends DataSource[ItemStack]{
+  collection.addNotified(this)
 
   override def getItem: ItemStack =
-    Option(map.toSeq)
+    Option(collection.toList)
       .filter(_.size > index)
       .map(i => visualize(i(index)))
       .orNull
-
-  override def onUpdate(): Unit = notifyAllObjects()
 }
 
-case class ConstSource(itemStack: ItemStack) extends DataSource {
+case class ConstSource(itemStack: ItemStack) extends DataSource[ItemStack] {
   override def getItem: ItemStack = itemStack
 }
 
-case class VariableSource[A](variable: A with Observable, visualize: A => ItemStack) extends DataSource with Notified {
+case class VariableSource[A](variable: A with Observable, visualize: A => ItemStack) extends DataSource[ItemStack]{
   variable.addNotified(this)
 
   override def getItem: ItemStack = visualize(variable)
-
-  override def onUpdate(): Unit = notifyAllObjects()
 }
 
-case class ListedSource[A, B](map: OpenObservable[A, B], pageSize: Int, visualize: ((A, B)) => ItemStack) extends Notified {
-  def pageCount = math.ceil(map.size.toDouble / pageSize).toInt
+case class ListedSource[A](collection: TraversableOnce[A] with Observable, pageSize: Int, visualize: A => ItemStack) extends DataSource[ArrayBuffer[A] with Observable]{
+  def pageCount: Int = math.ceil(collection.size.toDouble / pageSize).toInt
 
   private[this] var _page = 0
 
@@ -42,19 +38,17 @@ case class ListedSource[A, B](map: OpenObservable[A, B], pageSize: Int, visualiz
 
   def page_=(value: Int): Unit = {
     _page = value
-    updatePageContent()
-  }
-
-  val _pageMap = new mutable.OpenHashMap[A, B] with OpenObservable[A, B]
-  updatePageContent()
-
-  private def updatePageContent(): Unit = {
     _pageMap.clear()
-    _pageMap ++= map.toList.slice(page * pageSize, page * pageSize + pageSize)
+    _pageMap++= updatedPageContent()
+    onUpdate()
   }
 
+  private var _pageMap = new ArrayBuffer[A] with Observable
 
-  def pageMap: OpenObservable[A, B] = _pageMap
+  private def updatedPageContent(): List[A] = {
+    val pageStart = page * pageSize
+    collection.toList.slice(pageStart, pageStart + pageSize)
+  }
 
-  override def onUpdate(): Unit = _pageMap.notifyAllObjects()
+  override def getItem: ArrayBuffer[A] with Observable = _pageMap
 }

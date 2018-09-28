dd322ced1a842fa1c8d88e4d36b937a393b44547
hohserg <hohserg1@yandex.ru>
2018-09-28 22:50:26 +0300

    Added menu item factories

--- src/main/scala/hohserg/inventorymenu/menu/Decoration.scala
+++ src/main/scala/hohserg/inventorymenu/menu/Decoration.scala
@@ -3,3 +3,8 @@ package hohserg.inventorymenu.menu
 import org.bukkit.inventory.ItemStack
 
 case class Decoration(menu: Menu, x: Int, y: Int, source: DataSource[ItemStack]) extends MenuItem
+
+object Decoration {
+  def apply(x: Int, y: Int, source: DataSource[ItemStack]): Menu=>Decoration = new Decoration(_, x, y, source)
+  def apply(x: Int, y: Int, source: ItemStack): Menu=>Decoration = new Decoration(_, x, y, ConstSource(source))
+}

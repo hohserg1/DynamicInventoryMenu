dd0a3ff307e075a25430dfbda84420c7c83d93a4
hohserg <hohserg1@yandex.ru>
2018-09-28 22:53:59 +0300

    Refactoring

--- src/main/scala/hohserg/inventorymenu/menu/Button.scala
+++ src/main/scala/hohserg/inventorymenu/menu/Button.scala
@@ -1,10 +1,12 @@
 package hohserg.inventorymenu.menu
+
 import hohserg.inventorymenu.menu.Menu.ClickHandler
 import org.bukkit.inventory.ItemStack
 
 private[menu] case class Button(menu: Menu, x: Int, y: Int, source: DataSource[ItemStack], clickHandler: ClickHandler) extends MenuItem
 
 object Button {
-  def apply(x: Int, y: Int, source: DataSource[ItemStack], clickHandler: ClickHandler): Menu=>Button = new Button(_, x, y, source, clickHandler)
-  def apply(x: Int, y: Int, source: ItemStack, clickHandler: ClickHandler): Menu=>Button = new Button(_, x, y, ConstSource(source), clickHandler)
+  def apply(x: Int, y: Int, source: DataSource[ItemStack], clickHandler: ClickHandler): Menu => Button = new Button(_, x, y, source, clickHandler)
+
+  def apply(x: Int, y: Int, source: ItemStack, clickHandler: ClickHandler): Menu => Button = new Button(_, x, y, ConstSource(source), clickHandler)
 }

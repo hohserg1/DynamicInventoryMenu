cb1d9aa2302fd974e610e4956faee41763197a05
hohserg <hohserg1@yandex.ru>
2018-10-02 20:37:20 +0300

    Added check box button

---
+++ src/main/scala/hohserg/inventorymenu/menu/menuitems/CheckBox.scala
@@ -0,0 +1,25 @@
+package hohserg.inventorymenu.menu.menuitems
+
+import hohserg.inventorymenu.menu.{DataSource, Menu, VariableSource}
+import hohserg.inventorymenu.notify.Observable
+import org.bukkit.inventory.ItemStack
+
+case class CheckBox(menu: Menu, x: Int, y: Int, on: DataSource[ItemStack], off: DataSource[ItemStack], defaultState: Boolean = true) extends Clickable {
+  private[this] var _state: Boolean = defaultState
+
+  private val observable=new Object with Observable
+
+  private def state: Boolean = _state
+
+  private def state_=(value: Boolean): Unit = {
+    _state = value
+    observable.notifyAllObjects()
+  }
+
+  def switchState(): Any = state = !state
+
+  val clickHandler = _ => switchState()
+
+  override val source: DataSource[ItemStack] = VariableSource[Object](observable,_=>if(state)on.getItem else off.getItem)
+  source.addNotified(this)
+}

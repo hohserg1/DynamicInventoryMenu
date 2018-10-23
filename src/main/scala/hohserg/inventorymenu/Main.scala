a99c06ef795bb531895e88accbe4c911ee851cd7
hohserg <hohserg1@yandex.ru>
2018-10-23 18:00:37 +0300

    Extract examples

--- src/main/scala/hohserg/inventorymenu/Main.scala
+++ src/main/scala/hohserg/inventorymenu/Main.scala
@@ -1,45 +1,24 @@
 package hohserg.inventorymenu
 
-import java.util.concurrent.atomic.AtomicInteger
-import hohserg.inventorymenu.menu.ListView.Area
-import hohserg.inventorymenu.menu.menuitems.Button
-import hohserg.inventorymenu.menu.{ListView, Menu, MenuListener}
-import hohserg.inventorymenu.notify.OpenObservable
+import hohserg.inventorymenu.example.Examples
+import hohserg.inventorymenu.menu._
+import org.bukkit.Bukkit
 import org.bukkit.command.{Command, CommandSender}
 import org.bukkit.entity.Player
-import org.bukkit.inventory.ItemStack
-import org.bukkit.{Bukkit, DyeColor, Material}
-
-import scala.collection.mutable
 
 object Main {
+
   def onEnable(): Unit = {
     Bukkit.getPluginManager.registerEvents(new MenuListener, plugin)
+    Examples.onEnable()
   }
 
   def plugin: PluginPreloader = {
     PluginPreloader.instance
   }
 
-  val map = new mutable.OpenHashMap[String, Int] with OpenObservable[String, Int]
-  for (i <- 0 to 100) map += ("test" + i) -> 1
-
-  val tupleToStack: ((String, Int)) => ItemStack = {
-    case (name: String, i: Int) =>
-      val r = new ItemStack(Material.APPLE, i)
-      val meta = r.getItemMeta
-      meta.setDisplayName(name)
-      r.setItemMeta(meta)
-      r
-  }
-
-  val menu2 = Menu.applyOrCreate("TestListView",
-    new ListView(_, _, 5, map, tupleToStack, Area(1, 1, 7, 3))
-      .addScroll(0, 2, DyeColor.CYAN, ("Вверх", "Страница %d из %d", "Вниз"))
-  )
-  var i = 0
-
   def onCommand(sender: CommandSender, command: Command, label: String, args: Array[String]): Boolean = {
+    import hohserg.inventorymenu.example.Examples._
     sender match {
       case player: Player =>
         args.toList match {
@@ -47,7 +26,9 @@ object Main {
             map += ("test" + i) -> 1
             i += 1
           case _ =>
-            menu2(player).open()
+
+
+            menu1(player).open()
         }
       case _ =>
     }

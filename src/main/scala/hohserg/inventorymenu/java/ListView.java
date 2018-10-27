a0396ecb7f0529066d5d2857938f2f0bd8cd1642
hohserg <hohserg1@yandex.ru>
2018-10-28 01:13:21 +0300

    Update java ListView
    Added constructors and apply-es for java collections
    Added java-style functions for addition

--- src/main/scala/hohserg/inventorymenu/java/ListView.java
+++ src/main/scala/hohserg/inventorymenu/java/ListView.java
@@ -1,5 +1,7 @@
 package hohserg.inventorymenu.java;
 
+import hohserg.inventorymenu.java.notify.CollectionObservable;
+import hohserg.inventorymenu.java.notify.MapObservable;
 import hohserg.inventorymenu.menu.DataSource;
 import hohserg.inventorymenu.menu.Menu;
 import hohserg.inventorymenu.menu.menuitems.Decoration;
@@ -9,29 +11,49 @@ import org.bukkit.inventory.ItemStack;
 import scala.Function1;
 import scala.Function2;
 import scala.Function3;
+import scala.Tuple2;
 import scala.collection.JavaConverters;
+import scala.collection.Seq;
 import scala.collection.TraversableOnce;
 
 import java.util.Arrays;
 
-public class ListView extends hohserg.inventorymenu.menu.ListView{
+public class ListView<A> extends hohserg.inventorymenu.menu.ListView {
     public ListView(Player player, String name, int height,
-                    TraversableOnce collection,
-                    Function1 visualize,
+                    CollectionObservable<A> collection,
+                    Function1<A, ItemStack> visualize,
                     Area area,
                     ItemStack borderFiller,
-                    Function3<Object, Object, DataSource<ItemStack>,Function1<Menu, Decoration>> buttonFactory) {
-        super(player, name, height, collection, visualize, area, borderFiller, buttonFactory);
+                    Function3<Object, Object, DataSource<ItemStack>, Function1<Menu, Decoration>> buttonFactory) {
+        super(player, name, height, toScala(collection), visualize, area, borderFiller, buttonFactory);
     }
-    public static Function2<Player, String, ListView> apply(int height,
-                                                            TraversableOnce collection,
-                                                            Function1 visualize,
-                                                            Area area,
-                                                            ItemStack borderFiller,
-                                                            Function3<Object, Object, DataSource<ItemStack>,Function1<Menu, Decoration>> buttonFactory,
-                                                            Function1<hohserg.inventorymenu.menu.Menu, MenuItem>... menuItems){
-        return (player,title)->{
-            ListView r=new ListView(player,title,height,collection,
+
+    public <K, V> ListView(Player player, String name, int height,
+                           MapObservable<K, V> collection,
+                           Function1<A, ItemStack> visualize,
+                           Area area,
+                           ItemStack borderFiller,
+                           Function3<Object, Object, DataSource<ItemStack>, Function1<Menu, Decoration>> buttonFactory) {
+        super(player, name, height, toScala(collection), visualize, area, borderFiller, buttonFactory);
+    }
+
+    private static <K, V> TraversableOnce<Tuple2<K, V>> toScala(MapObservable<K, V> collection) {
+        return CollectionWithTraitConversions.convert(collection);
+    }
+
+    private static <A> TraversableOnce<A> toScala(CollectionObservable<A> collection) {
+        return CollectionWithTraitConversions.convert(collection);
+    }
+
+    public static <A> Function2<Player, String, ListView> apply(int height,
+                                                                CollectionObservable<A> collection,
+                                                                Function1<A, ItemStack> visualize,
+                                                                Area area,
+                                                                ItemStack borderFiller,
+                                                                Function3<Object, Object, DataSource<ItemStack>, Function1<Menu, Decoration>> buttonFactory,
+                                                                Function1<hohserg.inventorymenu.menu.Menu, MenuItem>... menuItems) {
+        return (Player player, String title) -> {
+            ListView<A> r = new ListView<>(player, title, height, collection,
                     visualize,
                     area,
                     borderFiller,
@@ -39,5 +61,31 @@ public class ListView extends hohserg.inventorymenu.menu.ListView{
             r.$plus$plus$eq(JavaConverters.asScalaBuffer(Arrays.asList(menuItems)).toSeq());
             return r;
         };
+    }
+
+    public static <K, V> Function2<Player, String, ListView> apply(int height,
+                                                                   MapObservable<K, V> collection,
+                                                                   Function1<Tuple2<K, V>, ItemStack> visualize,
+                                                                   Area area,
+                                                                   ItemStack borderFiller,
+                                                                   Function3<Object, Object, DataSource<ItemStack>, Function1<Menu, Decoration>> buttonFactory,
+                                                                   Function1<hohserg.inventorymenu.menu.Menu, MenuItem>... menuItems) {
+        return (Player player, String title) -> {
+            ListView<Tuple2<K, V>> r = new ListView<>(player, title, height, collection,
+                    visualize,
+                    area,
+                    borderFiller,
+                    buttonFactory);
+            r.$plus$plus$eq(JavaConverters.asScalaBuffer(Arrays.asList(menuItems)).toSeq());
+            return r;
+        };
+    }
+
+    public Menu add(Function1<Menu, MenuItem> button) {
+        return super.$plus$eq(button);
+    }
+
+    public Menu addAll(Seq<Function1<Menu, MenuItem>> button) {
+        return super.$plus$plus$eq(button);
     }
 }

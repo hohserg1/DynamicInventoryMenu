c61cdecb7fc8f90c6571c0e33a1f1ec3cf24920b
hohserg <hohserg1@yandex.ru>
2018-10-28 01:14:43 +0300

    Update java Menu
    Refactoring
    Added started menuItems
    Added java-style functions for addition

--- src/main/scala/hohserg/inventorymenu/java/Menu.java
+++ src/main/scala/hohserg/inventorymenu/java/Menu.java
@@ -5,19 +5,28 @@ import org.bukkit.entity.Player;
 import scala.Function1;
 import scala.Function2;
 import scala.collection.JavaConverters;
+import scala.collection.Seq;
 
 import java.util.Arrays;
 
-public class Menu extends hohserg.inventorymenu.menu.Menu{
+public class Menu extends hohserg.inventorymenu.menu.Menu {
     public Menu(Player player, String name, int height) {
         super(player, name, height);
     }
 
-    public static Function2<Player, String, Menu> apply(int height, Function1<hohserg.inventorymenu.menu.Menu, MenuItem>... menuItems){
-        return (player,title)->{
-            Menu r=new Menu(player,title,height);
+    public static Function2<Player, String, Menu> apply(int height, Function1<hohserg.inventorymenu.menu.Menu, MenuItem>... menuItems) {
+        return (player, title) -> {
+            Menu r = new Menu(player, title, height);
             r.$plus$plus$eq(JavaConverters.asScalaBuffer(Arrays.asList(menuItems)).toSeq());
             return r;
         };
+    }
+
+    public hohserg.inventorymenu.menu.Menu add(Function1<hohserg.inventorymenu.menu.Menu, MenuItem> button) {
+        return super.$plus$eq(button);
+    }
+
+    public hohserg.inventorymenu.menu.Menu addAll(Seq<Function1<hohserg.inventorymenu.menu.Menu, MenuItem>> button) {
+        return super.$plus$plus$eq(button);
     }
 }

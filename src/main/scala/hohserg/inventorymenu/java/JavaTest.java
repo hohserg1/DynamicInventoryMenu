3119418f4eb8f48f1f951503fb2cb1d55c415fe8
hohserg <hohserg1@yandex.ru>
2018-11-29 18:21:12 +0300

    Core4ct some things
    beautify code
    correct MenuFactory type

--- src/main/scala/hohserg/inventorymenu/java/JavaTest.java
+++ src/main/scala/hohserg/inventorymenu/java/JavaTest.java
@@ -4,12 +4,14 @@ import hohserg.inventorymenu.menu.menuitems.Button;
 import hohserg.inventorymenu.menu.menuitems.Clickable;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 import scala.Function1;
+import scala.Function3;
 
 public class JavaTest {
     MenuFactory<Menu> menu1 = Menu.applyOrCreate(Menu.apply("Test", 5, Button.apply(0, 0, new ItemStack(Material.APPLE),
-            (player, button,clickType) -> {
+            (player, button, clickType) -> {
                 //click
                 return null;
             }
--- src/main/scala/hohserg/inventorymenu/java/MenuFactory.scala
+++ src/main/scala/hohserg/inventorymenu/java/MenuFactory.scala
@@ -2,6 +2,6 @@ package hohserg.inventorymenu.java
 
 import org.bukkit.entity.Player
 
-abstract class MenuFactory[A] extends ((String, Player) => A) {
+abstract class MenuFactory[A] extends (Player => A) {
 
 }

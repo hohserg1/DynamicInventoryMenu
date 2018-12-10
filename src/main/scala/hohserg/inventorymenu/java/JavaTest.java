d0fc9b051f643232c08aba49e913f968163c0bcf
hohserg <hohserg1@yandex.ru>
2018-12-10 15:35:44 +0300

    Correct examples

--- src/main/scala/hohserg/inventorymenu/java/JavaTest.java
+++ src/main/scala/hohserg/inventorymenu/java/JavaTest.java
@@ -2,6 +2,7 @@ package hohserg.inventorymenu.java;
 
 import hohserg.inventorymenu.menu.menuitems.Button;
 import hohserg.inventorymenu.menu.menuitems.Clickable;
+import hohserg.inventorymenu.menu.menuitems.ImplicitUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
@@ -10,12 +11,15 @@ import scala.Function1;
 import scala.Function3;
 
 public class JavaTest {
-    MenuFactory<Menu> menu1 = Menu.applyOrCreate(Menu.apply("Test", 5, Button.apply(0, 0, new ItemStack(Material.APPLE),
-            (player, button, clickType) -> {
-                //click
-                return null;
-            }
-    )));
+
+    MenuFactory<Menu> menu1 = Menu.applyOrCreate(Menu.apply("Test", 5,
+            Button.apply(0, 0,
+                    ImplicitUtils.stack2source(new ItemStack(Material.APPLE)),
+                    ImplicitUtils.clickHandler2partialClickHandler(clickEvent -> {
+                        //click
+                        return null;
+                    })
+            )));
     //MenuFactory<ListView> menu2=Menu.applyOrCreate("Test",ListView.apply(5));
 
     public JavaTest() {
